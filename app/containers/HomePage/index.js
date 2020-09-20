99b5b9cd871143a350fe02aad9bc44c4ca388576
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-09-19 20:24:51 -0700

    Initial commit

---
+++ app/containers/HomePage/index.js
@@ -0,0 +1,20 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ *
+ */
+
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
+
+export default function HomePage(props) {
+  return (
+    <>
+      <h1>
+        <FormattedMessage {...messages.header} />
+      </h1>
+    </>
+  );
+}
---
+++ app/global-styles.js
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    height: 100%;
+    width: 100%;
+    line-height: 1.5;
+  }
+
+  body {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+
+  body.fontLoaded {
+    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+
+  #app {
+    background-color: #fafafa;
+    min-height: 100%;
+    min-width: 100%;
+  }
+
+  p,
+  label {
+    font-family: Georgia, Times, 'Times New Roman', serif;
+    line-height: 1.5em;
+  }
+  
+  .todo-header-logo img {
+    width:40px;
+    position:absolute;
+    top:-5px;
+  }
+`;
+
+export default GlobalStyle;
