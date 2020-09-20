e4c6ae43973528e267b2010fe67092c4ff89c12f
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-09-19 22:19:13 -0700

    Add ToDoTabs component

---
+++ app/containers/ToDo/ToDoTabs.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ToDoTabs = ({ match }) => {
+
+    const navLists = [
+        {
+            name: "My List",
+            path: "MyList",
+            icon: "fa fa-list"
+        }, {
+            name: "Recipes",
+            path: "Recipes",
+            icon: "fa fa-search"
+        }, {
+            name: "My Recipes",
+            path: "MyRecipes",
+            icon: "fa fa-heart"
+        }, {
+            name: "Meal Plan",
+            path: "MealPlan",
+            icon: "fa fa-heart"
+        }
+    ];
+
+    const checkActiveTabs = tab => {
+        return match.params.tab === tab ? 'active' : '';
+    }
+
+    const mapTabs = () => {
+        return (
+            <div className="bottom-shadow nav-app">
+                <ul className="nav nav-pills hideScroll">
+                    {navLists.map((tab, index) => {
+                        return <Link key={index} className={`nav-link ${checkActiveTabs(tab.path)}`} to={tab.path}><i className={tab.icon}></i> {tab.name}</Link>
+                    })}
+                </ul>
+            </div>
+        );
+    }
+    return (mapTabs());
+}
+
+export default ToDoTabs;
+
+
