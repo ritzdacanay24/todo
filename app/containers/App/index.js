6908cdccd0c7e1adc91d44f8c76f2c65ab1be50c
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-09-19 20:33:02 -0700

    Imported 3rd party css packages, components, and set navigation routes

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -13,16 +13,38 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
+//custom
+import ToDo from 'containers/ToDo/Loadable';
+import Profile from 'containers/Profile/Loadable';
+import Features from 'containers/Features/Loadable';
+import SignIn from 'containers/SignIn/Loadable';
+import NavbarHeader from "../ToDo/NavbarHeader";
+import Footer from "../ToDo/Footer";
+
+//main app css
 import GlobalStyle from '../../global-styles';
 
+//3rd party css
+import "font-awesome/css/font-awesome.css";
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import 'react-notifications/lib/notifications.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
     <div className="App">
+      <NavbarHeader />
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/ToDo/" component={ToDo} />
+        <Route exact path="/ToDo/:id" component={ToDo} />
+        <Route exact path="/ToDo/:id/:tab" component={ToDo} />
+        <Route exact path="/Profile" component={Profile} />
+        <Route exact path="/Features" component={Features} />
+        <Route exact path="/SignIn" component={SignIn} />
         <Route component={NotFoundPage} />
       </Switch>
+      <Footer />
       <GlobalStyle />
     </div >
   );
