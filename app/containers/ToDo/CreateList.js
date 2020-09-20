cda3048e5b0b80371dc1cc00dc813bbc77f8cb9d
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-09-19 22:20:59 -0700

    Add CreateList Component

---
+++ app/containers/ToDo/CreateList.js
@@ -0,0 +1,59 @@
+import React, { useState, useRef } from "react";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+const CreateList = ({ createNewList }) => {
+
+    const [show, setShow] = useState(false);
+    const [listName, setListName] = useState("");
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const onSubmit = i => {
+        createNewList(listName);
+        setShow(false)
+        setListName("")
+    }
+
+    //autofocus when model opens
+    const inputRef = useRef(null);
+    if (show) {
+        setTimeout(() => {
+            inputRef.current.focus();
+        }, 1);
+    }
+
+    const handleOnChange = ev => {
+        setListName(ev.target.value)
+    }
+
+    return (
+        <>
+            <Button className="btn-orange" onClick={handleShow}> <i className="fa fa-plus" aria-hidden="true"></i> Add List </Button>
+
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title> <i className="fa fa-list" aria-hidden="true"></i> New List</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="listName">
+                            <Form.Label>List Name</Form.Label>
+                            <Form.Control name="name" type="text" ref={inputRef} autoComplete="off" placeholder="Enter list name" className="form-control" onChange={ev => handleOnChange(ev)} />
+                            <Form.Text className="text-muted">
+                                You can name the list whatever you want
+                        </Form.Text>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}> Close </Button>
+                    <Button variant="primary" onClick={onSubmit}> Submit </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+export default CreateList;
