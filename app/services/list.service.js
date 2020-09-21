20191560bd654a8d4a5fa88eb200a8ad290d3bb0
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-09-21 12:59:31 -0700

    Add list service which provides all CRUD operations

---
+++ app/services/list.service.js
@@ -0,0 +1,23 @@
+import API from "../utils/API";
+
+class ListService {
+    
+  async createNewList(data) {
+    return await API.post(`lists/`, data);
+  }
+
+  async updateList(list) {
+    return await API.put(`lists/${list._id}`, list);
+  }
+
+  async deleteListById(listId) {
+    return await API.delete(`lists/${listId}`);
+  }
+
+  async getListByUserId(userId) {
+    return await API.get(`lists/${userId}`);
+  }
+  
+}
+
+export default new ListService();
