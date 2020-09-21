23de1047f4cbb4bf93b3944bbfc3f46c6e6b94ca
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-09-21 12:59:56 -0700

    Add recipe service which provides all CRUD operations

---
+++ app/services/recipe.service.js
@@ -0,0 +1,23 @@
+import API from "../utils/API";
+
+class RecipeService {
+    
+  async addRecipeAction(params) {
+    return await API.post(`recipes/`, params);
+  }
+
+  async deleteRecipeById(recipeId) {
+    return await API.delete(`recipes/${recipeId}`);
+  }
+
+  async getRecipeByUserId(userId) {
+    return await API.get(`recipes/${userId}`);
+  }
+
+  async getRecipeById(recipeId) {
+    return await API.get(`recipes/${recipeId}/recipeId`);
+  }
+  
+}
+
+export default new RecipeService();
