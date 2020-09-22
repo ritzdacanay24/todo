39ae1bad105a72ecf4b3e80ecc3a6eabb706bc74
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-09-21 21:54:17 -0700

    Add meal plan component to view random recipes for entire week

---
+++ app/components/ToDo/MealPlan.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import SpoonacularService from '../../services/spoonacular.service';
+
+const MealPlan = props => {
+    const [meals, setMeals] = useState([])
+
+    useEffect(() => {
+        async function getBulk(ids) {
+            let params = { ids: ids }
+            const res = await SpoonacularService.getInformationBulk(params);
+            setMeals(res.data);
+        }
+        const fetchData = async function fetchData() {
+            const res = await SpoonacularService.getMealPlan();
+            let idArray = []
+            res.data.items.map(list => {
+                let value = JSON.parse(list.value);
+                idArray.push(value.id)
+            })
+            getBulk(idArray.toString());
+        }
+        fetchData();
+    }, []);
+
+    const mapMeals = () => {
+        return (
+            <div className="row">
+                {meals.map((item, index) => {
+                    return (
+                        <div className="col-lg-4" key={index}>
+                            <Card>
+                                <Card.Img variant="top" src={item.image} />
+                                <Card.Body>
+                                    <Card.Title>Day {index % 3 + 1}</Card.Title>
+                                    <Card.Text>
+                                        {item.title}
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <small className="text-muted">View</small>
+                                </Card.Footer>
+                            </Card>
+                        </div>
+                    );
+                })}
+            </div>
+        )
+    }
+
+    return (mapMeals());
+}
+
+export default MealPlan;
