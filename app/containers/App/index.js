912658eab8fc243cc781ba2af46c296b278892b9
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-09-21 21:49:14 -0700

    Pass props to components, update file path and axios call to get userInfo

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,19 +7,20 @@
  *
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
 //custom
-import ToDo from 'containers/ToDo/Loadable';
-import Profile from 'containers/Profile/Loadable';
-import Features from 'containers/Features/Loadable';
-import SignIn from 'containers/SignIn/Loadable';
-import NavbarHeader from "../ToDo/NavbarHeader";
-import Footer from "../ToDo/Footer";
+import ToDo from '../../components/ToDo';
+import Profile from "../../components/Profile/Profile";
+import Features from '../../components/Features';
+import NavbarHeader from "../../components/NavbarHeader";
+import Footer from "../../components/Footer";
+import Login from "../../components/login.component";
+import Register from "../../components/register.component";
 
 //main app css
 import GlobalStyle from '../../global-styles';
@@ -30,24 +31,53 @@ import 'react-confirm-alert/src/react-confirm-alert.css';
 import 'react-notifications/lib/notifications.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <NavbarHeader />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/ToDo/" component={ToDo} />
-        <Route exact path="/ToDo/:id" component={ToDo} />
-        <Route exact path="/ToDo/:id/:tab" component={ToDo} />
-        <Route exact path="/Profile" component={Profile} />
-        <Route exact path="/Features" component={Features} />
-        <Route exact path="/SignIn" component={SignIn} />
-        <Route component={NotFoundPage} />
-      </Switch>
-      <Footer />
-      <GlobalStyle />
-    </div >
-  );
+import AuthService from "../../services/auth.service";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showModeratorBoard: false,
+      showAdminBoard: false,
+      currentUser: undefined,
+      isAuthenticating: true
+    };
+  }
+
+  async componentDidMount() {
+    const user = await AuthService.getCurrentUser();
+    if (user) {
+      this.setState({
+        currentUser: user
+      });
+    }
+    this.setState({isAuthenticating:false})
+  }
+  
+  render() {
+    if (this.state.isAuthenticating) return null;
+    const { currentUser } = this.state;
+    return (
+      <div className="App">
+        <NavbarHeader userInfo={currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/ToDo" render={(props) => (<ToDo currentUser={currentUser} {...props}/>)}  />
+          <Route exact path="/ToDo/:id" render={(props) => (<ToDo currentUser={currentUser} {...props}/>)}  />
+          <Route exact path="/ToDo/:id/:tab" render={(props) => (<ToDo currentUser={currentUser} {...props}/>)}  />
+          <Route exact path="/Profile" render={(props) => (<Profile currentUser={currentUser} {...props}/>)}  />
+          <Route exact path="/Features" component={Features} />
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/Register" component={Register} />
+          <Route component={NotFoundPage} />
+        </Switch>
+        <Footer />
+        <GlobalStyle />
+      </div >
+    )
+  };
 }
 
 export default App;
