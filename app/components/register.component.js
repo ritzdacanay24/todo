9b1409a4de8809514dc79c981dea9b7c0cf819c3
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-09-22 23:33:13 -0700

    Add few css

--- app/components/register.component.js
+++ app/components/register.component.js
@@ -103,108 +103,109 @@ export default class Register extends Component {
 
   render() {
     return (
-
-      <div className="container" style={{ paddingTop: "80px" }}>
-        <div className="row justify-content-md-center">
-          <div className="col-md-7">
-            <Card>
-              <Card.Header className="text-center" style={{ borderBottom: "none" }}>
-                <img src={check} style={{ width: "250px" }} />
-              </Card.Header>
-              <Card.Body>
-                <Card.Title className="text-center">Register</Card.Title>
-                <Form
-                  onSubmit={this.handleRegister}
-                  ref={c => {
-                    this.form = c;
-                  }}
-                >
-                  {!this.state.successful && (
-                    <div>
-
-                      <div className="form-group">
-                        <label htmlFor="firstName">First name</label>
-                        <Input
-                          type="text"
-                          className="form-control"
-                          name="firstName"
-                          value={this.state.firstName}
-                          onChange={this.onChangeForm}
-                          validations={[required]}
-                        />
-                      </div>
-                      <div className="form-group">
-                        <label htmlFor="lastName">Last name</label>
-                        <Input
-                          type="text"
-                          className="form-control"
-                          name="lastName"
-                          value={this.state.lastName}
-                          onChange={this.onChangeForm}
-                          validations={[required]}
-                        />
-                      </div>
-                      <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <Input
-                          type="text"
-                          className="form-control"
-                          name="email"
-                          value={this.state.email}
-                          onChange={this.onChangeForm}
-                          validations={[required, email]}
-                        />
-                      </div>
-
-                      <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <Input
-                          type="password"
-                          className="form-control"
-                          name="password"
-                          value={this.state.password}
-                          onChange={this.onChangeForm}
-                          validations={[required, vpassword]}
-                        />
-                      </div>
-
-                      <div className="form-group">
-                        <button className="btn btn-primary btn-block">Sign Up</button>
-                      </div>
-                      <Link to="/Login">Already have an account</Link>
-                    </div>
-                  )}
-
-                  {this.state.message && (
-                    <div className="form-group">
-                      <div
-                        className={
-                          this.state.successful
-                            ? "alert alert-success"
-                            : "alert alert-danger"
-                        }
-                        role="alert"
-                      >
-                        {this.state.message}
-                      </div>
-                      <div className="form-group">
-                        <Link className="btn btn-primary btn-block" to="/Login">Log in</Link>
-                      </div>
-                    </div>
-
-                  )}
-                  <CheckButton
-                    style={{ display: "none" }}
+      <header className="register">
+        <div className="container">
+          <div className="row justify-content-md-center">
+            <div className="col-md-7">
+              <Card className="text-center shadow">
+                <Card.Header className="text-center" style={{ borderBottom: "none" }}>
+                  <img src={check} style={{ width: "250px" }} />
+                </Card.Header>
+                <Card.Body>
+                  <Card.Title className="text-center">Register</Card.Title>
+                  <Form
+                    onSubmit={this.handleRegister}
                     ref={c => {
-                      this.checkBtn = c;
+                      this.form = c;
                     }}
-                  />
-                </Form>
-              </Card.Body>
-            </Card>
+                  >
+                    {!this.state.successful && (
+                      <div>
+
+                        <div className="form-group">
+                          <label htmlFor="firstName">First name</label>
+                          <Input
+                            type="text"
+                            className="form-control"
+                            name="firstName"
+                            value={this.state.firstName}
+                            onChange={this.onChangeForm}
+                            validations={[required]}
+                          />
+                        </div>
+                        <div className="form-group">
+                          <label htmlFor="lastName">Last name</label>
+                          <Input
+                            type="text"
+                            className="form-control"
+                            name="lastName"
+                            value={this.state.lastName}
+                            onChange={this.onChangeForm}
+                            validations={[required]}
+                          />
+                        </div>
+                        <div className="form-group">
+                          <label htmlFor="email">Email</label>
+                          <Input
+                            type="text"
+                            className="form-control"
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.onChangeForm}
+                            validations={[required, email]}
+                          />
+                        </div>
+
+                        <div className="form-group">
+                          <label htmlFor="password">Password</label>
+                          <Input
+                            type="password"
+                            className="form-control"
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.onChangeForm}
+                            validations={[required, vpassword]}
+                          />
+                        </div>
+
+                        <div className="form-group">
+                          <button className="btn btn-primary btn-block">Sign Up</button>
+                        </div>
+                        <Link to="/Login">Already have an account</Link>
+                      </div>
+                    )}
+
+                    {this.state.message && (
+                      <div className="form-group">
+                        <div
+                          className={
+                            this.state.successful
+                              ? "alert alert-success"
+                              : "alert alert-danger"
+                          }
+                          role="alert"
+                        >
+                          {this.state.message}
+                        </div>
+                        <div className="form-group">
+                          <Link className="btn btn-primary btn-block" to="/Login">Log in</Link>
+                        </div>
+                      </div>
+
+                    )}
+                    <CheckButton
+                      style={{ display: "none" }}
+                      ref={c => {
+                        this.checkBtn = c;
+                      }}
+                    />
+                  </Form>
+                </Card.Body>
+              </Card>
+            </div>
           </div>
         </div>
-      </div>
+      </header>
     );
   }
 }
