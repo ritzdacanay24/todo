8e76a84c1a5f947583a1f14df4129243837c6562
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-09-23 19:30:42 -0700

    Add protected route component to check if user has logged in for certain components

---
+++ app/containers/ProtectedRoute.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const ProtectedRoute = ({ component: Component, currentUser, ...rest }) => {
+  return (
+    <Route {...rest} render={
+        props => {
+          if (currentUser) {
+            return <Component {...rest} {...props} currentUser={currentUser}/>
+          } else {
+            return <Redirect to={
+              {
+                pathname: '/',
+                state: {
+                  from: props.location
+                }
+              }
+            } />
+          }
+        }
+      } />
+  )
+}
+
+export default ProtectedRoute;
