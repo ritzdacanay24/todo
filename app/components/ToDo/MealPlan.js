75fb60ed81a8c457acc05ca21045b8b6ee1ebb49
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-09-24 20:19:06 -0700

    Add laoding indicator as this is a lengthy http call

--- app/components/ToDo/MealPlan.js
+++ app/components/ToDo/MealPlan.js
@@ -5,14 +5,17 @@ const repo = new RepositoryWrapper();
 
 const MealPlan = props => {
     const [meals, setMeals] = useState([])
+    const [isLoading, setLoading] = useState(false)
 
     useEffect(() => {
         async function getBulk(ids) {
             let params = { ids: ids }
             const res = await repo.SpoonacularService.getInformationBulk(params);
             setMeals(res.data);
+            setLoading(false)
         }
         const fetchData = async function fetchData() {
+            setLoading(true)
             const res = await repo.SpoonacularService.getMealPlan();
             let idArray = []
             res.data.items.map(list => {
@@ -26,26 +29,29 @@ const MealPlan = props => {
 
     const mapMeals = () => {
         return (
-            <div className="row">
-                {meals.map((item, index) => {
-                    return (
-                        <div className="col-lg-4" key={index}>
-                            <Card>
-                                <Card.Img variant="top" src={item.image} />
-                                <Card.Body>
-                                    <Card.Title>Day {index % 3 + 1}</Card.Title>
-                                    <Card.Text>
-                                        {item.title}
-                                    </Card.Text>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <small className="text-muted">View</small>
-                                </Card.Footer>
-                            </Card>
-                        </div>
-                    );
-                })}
-            </div>
+            <>
+                {isLoading && <p><i className="fa fa-spinner fa-spin fa-fw"></i> Please wait while we build your meal plan. Thank you </p>}
+                <div className="row">
+                    {meals.map((item, index) => {
+                        return (
+                            <div className="col-lg-4" key={index}>
+                                <Card>
+                                    <Card.Img variant="top" src={item.image} />
+                                    <Card.Body>
+                                        <Card.Title>Day {index % 3 + 1}</Card.Title>
+                                        <Card.Text>
+                                            {item.title}
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <small className="text-muted">View</small>
+                                    </Card.Footer>
+                                </Card>
+                            </div>
+                        );
+                    })}
+                </div>
+            </>
         )
     }
 
