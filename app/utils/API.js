8dd4709afa79f0382439e5a83e0f8b33aee85c59
Ritz Dacanay <ritzdacanay24@yahoo.com>
2020-09-28 09:39:12 -0700

    Add axiox response instance to catch errorsgitasdfsdc

--- app/utils/API.js
+++ app/utils/API.js
@@ -1,4 +1,5 @@
 import Axios from "axios";
+import { NotificationManager } from 'react-notifications';
 
 const fetchClient = () => {
   const defaultOptions = {
@@ -17,6 +18,20 @@ const fetchClient = () => {
     if (token) config.headers['x-auth-token'] = token.appToken;
     return config;
   });
+
+  instance.interceptors.response.use(undefined, error => {
+    if (error.message === 'Network Error' && !error.response) {
+      NotificationManager.error('Network error - Please make sure API is running');
+    }
+    const { status, data, config } = error.response;
+
+    if (status == 500) {
+      NotificationManager.error('Server error - Check the terminal for more info.');
+    }
+    if (status == 404) {
+      NotificationManager.error('Server error - API end point cannot be found. ');
+    }
+  })
 
   return instance;
 };
